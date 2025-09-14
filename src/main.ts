b75c0314264dec1859fd6ea5ced92bcc576d14d5
Mario Tarosso <mariojgt2@gmail.com>
2025-09-14 22:22:23 +0100

    Revert "first push"
    This reverts commit 9ce57b9d9ea13eed4a2b665be6168c0e5fb90024.

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-
-import { createApp } from 'vue'
-import { IonicVue } from '@ionic/vue'
-import App from './App.vue'
-import router from './router'
-import './styles/main.css'
-const app = createApp(App).use(IonicVue).use(router)
-router.isReady().then(() => app.mount('#app'))
